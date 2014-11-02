8bc6c20a3abb9e4b7cb0833f56e85ac7769fd700
NathanHardy <jonathanhardy9001@gmail.com>
2014-11-01 21:48:40 -0400

    Created More Activities and Fragments

--- NotMyMusic/app/src/main/java/com/nathan/hardy/notmymusic/DetailActivity.java
+++ NotMyMusic/app/src/main/java/com/nathan/hardy/notmymusic/DetailActivity.java
@@ -3,7 +3,6 @@ package com.nathan.hardy.notmymusic;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
-import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -13,14 +12,12 @@ import android.view.ViewGroup;
 import android.os.Build;
 
 
-public class MainActivity extends Activity {
-
-    SearchFragment searchFragment = new SearchFragment();
+public class DetailActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_detail);
         if (savedInstanceState == null) {
             getFragmentManager().beginTransaction()
                     .add(R.id.container, new PlaceholderFragment())
@@ -32,7 +29,7 @@ public class MainActivity extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_detail, menu);
         return true;
     }
 
@@ -44,11 +41,7 @@ public class MainActivity extends Activity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_search) {
-            return true;
-        }
-
-        if (id == R.id.action_add) {
+        if (id == R.id.action_settings) {
             return true;
         }
 
@@ -66,14 +59,8 @@ public class MainActivity extends Activity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
             return rootView;
         }
-    }
-
-    public void SwapView(View view) {
-        FragmentTransaction transaction = getFragmentManager().beginTransaction().addToBackStack(null);
-
-        transaction.remove(searchFragment);
     }
 }
